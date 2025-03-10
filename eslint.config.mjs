3db91e3b1b3e3fe9427b4f61757489dc4aa09dcb
kabutoa <yinyun957@163.com>
2025-03-10 11:39:47 +0800

    feat: 初始化eslint和prettier

---
+++ eslint.config.mjs
@@ -0,0 +1,41 @@
+import { FlatCompat } from "@eslint/eslintrc";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+});
+
+const eslintConfig = [
+  ...compat.extends("next/core-web-vitals", "next/typescript", "prettier"),
+  ...compat.plugins("check-file", "n"),
+  ...compat.config({
+    rules: {
+      "prefer-arrow-callback": "error",
+      "prefer-template": "error",
+      semi: "error",
+      quotes: "error",
+      "n/no-process-env": "error",
+      "check-file/filename-naming-convention": [
+        "error",
+        {
+          "**/*.{ts,tsx}": "KEBAB_CASE",
+        },
+        {
+          ignoreMiddleExtensions: true,
+        },
+      ],
+      "check-file/folder-naming-convention": [
+        "error",
+        {
+          "src/**/!^[.*": "KEBAB_CASE",
+        },
+      ],
+    },
+  }),
+];
+
+export default eslintConfig;
